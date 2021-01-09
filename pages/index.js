2a93a6fa99881af90948139c0aa88538e1b5ddf5
Gabriel Florit <gabrielflorit@gmail.com>
2021-01-08 22:23:24 -0500

    Remove unused files

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>SCRIPT-8 v2</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>SCRIPT-8 v2</h1>
+      <main>
+        <h1>SCRIPT-8 v2</h1>
 
         <p>
           This is the SCRIPT-8 rewrite. Follow along at{' '}
